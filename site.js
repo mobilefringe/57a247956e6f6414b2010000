00a13ca038da4fb75e7a8e07ba09774688409fc9
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2016-08-05 15:49:48 -0400

    Alameda Crossing 1.0  /site.js

---
+++ site.js
@@ -0,0 +1,6 @@
+var site_json = {
+                    "name" : "Long Beach Towne Center",
+                    "default_image" : "http://assets.codecloudapp.com/sites/57a247956e6f6414b2010000/image/jpeg/undefined/al_500x500_logo.jpg",
+                    "time_zone" : "T08:00:00Z",
+                    "social_feed" : "http://longbeach.mallmaverick.com/api/v2/longbeach/social.json"
+                }
