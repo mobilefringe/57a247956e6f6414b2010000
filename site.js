d7d80047c695d9a908d0ace3ba8fd9f15809a368
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2016-10-04 11:21:41 -0400

    Alameda Crossing 1.1  /site.js

--- site.js
+++ site.js
@@ -1,6 +1,6 @@
 var site_json = {
                     "name" : "Long Beach Towne Center",
-                    "default_image" : "//assets.codecloudapp.com/sites/57f2b41c6e6f645d03af0000/image/jpeg/undefined/al_500x500_logo.jpg",
+                    "default_image" : "//codecloud.cdn.speedyrails.net/sites/57f2b41c6e6f645d03af0000/image/jpeg/undefined/al_500x500_logo.jpg",
                     "time_zone" : "T08:00:00Z",
                     "social_feed" : "//alamedacrossing.mallmaverick.com/api/v2/alamedacrossing/social.json"
                 }
