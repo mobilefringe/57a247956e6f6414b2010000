22fa306d4ee2d2b4b0e48d73dc43ab6ea9b88c32
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-05 10:55:36 -0500

    Alameda Crossing 1.4  /site.js

---
+++ site.js
@@ -0,0 +1,9 @@
+var site_json = {
+                    "name" : "Alameda Landing",
+                    "default_image" : "//codecloud.cdn.speedyrails.net/sites/57f2b41c6e6f645d03af0000/image/jpeg/undefined/al_500x500_logo.jpg",
+                    "time_zone" : "T08:00:00Z",
+                    "social_feed" : "//alamedacrossing.mallmaverick.com/api/v2/alamedacrossing/social.json"
+                }
+                
+var current_year = moment().year();
+$("#current_year").text(current_year);
