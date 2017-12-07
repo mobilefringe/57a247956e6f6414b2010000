c31526a41fb29634e4d8d00d051653070eba1487
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-07 10:33:56 -0500

    Alameda Crossing 1.4  /site.js

--- site.js
+++ site.js
@@ -1,10 +1,3 @@
-$( document ).ready(function() {
-    var current_year = moment().year();
-    console.log(current_year)
-    $("#current_year").text(current_year);
-});
-
-
 var site_json = {
                     "name" : "Alameda Landing",
                     "default_image" : "//codecloud.cdn.speedyrails.net/sites/57f2b41c6e6f645d03af0000/image/jpeg/undefined/al_500x500_logo.jpg",
